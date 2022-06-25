eef03403c8a56e47761fdc88081677c5a21b695d
skyxcripto <producer@skynance.net>
2022-06-25 12:41:51 -0300

    feat(finish menu header): finish Header component

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,11 @@ export default class MyDocument extends Document {
         <Head>{/*  Add font files or html attributes here. */}</Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         {/* @ts-ignore */}
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap"
           rel="stylesheet"
